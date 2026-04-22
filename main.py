ec25a30e3529ccb0163301fbb05e2c42e6021d46
Katrin <katabrovkova952@gmail.com>
2026-04-22 15:35:42 +0400

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,79 @@
-import this
-numbers = [123, 21, 89, 76, 45]
-print("Numbers: ", min(numbers), max(numbers))
+# def click():
+#     print("Hello")
+# click()
+
+# def add(c, f):
+#     return c % f
+# print(add(10,5))
+
+# def double(n):
+#     return n * 2
+# x = double(3)
+# y = double(5)
+# print(x + y)
+
+# numbers = [5, 10, 3, 7]
+# total = 0
+# for number in numbers:
+#     total += number
+# print(total)
+
+# numbers = [8, 2, 15, 4 , 9]
+# max_num = numbers[0]
+# for num in numbers:
+#     if num > max_num:
+#         max_num = num
+# print(max_num)
+
+# numbers = [12, 4, 8, 1, 9]
+# max_num = numbers[0]
+# min_num = numbers[0]
+# for num in numbers:
+#     if num > max_num:
+#         max_num = num
+#
+#     if num < min_num:
+#         min_num = num
+# print("Max:", max_num)
+# print("Min:", min_num)
+
+# numbers = []
+# while True:
+#     value = input(Enter your number:)
+#
+#     if value == "stop":
+#         break
+#
+#     number = int(value)
+#     numbers.append(number)
+#
+# total = 0
+# for num in numbers:
+#     total += num
+#
+# print("Numbers:", numbers)
+# print("Total:", total)
+
+# numbers = [7, 3, 12, 1, 9]
+# min_number = numbers[0]
+# max_number = numbers[0]
+#
+# def square(n):
+#     return n * n
+# print(square(4))
+# print(square(6))
+
+# numbers = [2, 5, 8, 3]
+# for num in numbers:
+#     if num > 4:
+#       print(num*2)
+
+def get_max(numbers):
+    max_number = numbers[0]
+    for num in numbers:
+         if num > max_number:
+             max_number = num
+
+    return max(numbers)
+
+print(get_max([12, 9, 2, 9, 589, 1009]))
