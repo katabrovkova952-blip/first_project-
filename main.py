4a8b94c5c9e25b46162739e140db707ac7cf3555
Katrin <katabrovkova952@gmail.com>
2026-05-12 13:01:49 +0400

    created a game

--- main.py
+++ main.py
@@ -1,367 +1,53 @@
-# def click():
-#     print("Hello")
-# click()
-
-
-# def add(c, f):
-#     return c % f
-# print(add(10,5))
-
-# def double(n):
-#     return n * 2
-# x = double(3)
-# y = double(5)
-# print(x + y)
-
-# numbers = [5, 10, 3, 7]
-# total = 0
-# for number in numbers:
-#     total += number
-# print(total)
-
-# numbers = [8, 2, 15, 4 , 9]
-# max_num = numbers[0]
-# for num in numbers:
-#     if num > max_num:
-#         max_num = num
-# print(max_num)
-
-# numbers = [12, 4, 8, 1, 9]
-# max_num = numbers[0]
-# min_num = numbers[0]
-# for num in numbers:
-#     if num > max_num:
-#         max_num = num
-#
-#     if num < min_num:
-#         min_num = num
-# print("Max:", max_num)
-# print("Min:", min_num)
-
-# numbers = []
-# while True:
-#     value = input(Enter your number:)
-#
-#     if value == "stop":
-#         break
-#
-#     number = int(value)
-#     numbers.append(number)
-#
-# total = 0
-# for num in numbers:
-#     total += num
-#
-# print("Numbers:", numbers)
-# print("Total:", total)
-
-# numbers = [7, 3, 12, 1, 9]
-# min_number = numbers[0]
-# max_number = numbers[0]
-#
-# def square(n):
-#     return n * n
-# print(square(4))
-# print(square(6))
-
-# numbers = [2, 5, 8, 3]
-# for num in numbers:
-#     if num > 4:
-#       print(num*2)
-
-# def get_max(numbers):
-#     max_number = numbers[0]
-#     for num in numbers:
-#          if num > max_number:
-#              max_number = num
-#
-#     return max(numbers)
-#
-# print(get_max([12, 9, 2, 9, 589, 1009]))
-
-# numbers = []
-# total = 0
-# while True:
-#     value = input("Enter a numbers:")
-#     if value == "stop":
-#         break
-#     number = int(value)
-#     numbers.append(number)
-#
-# min_num = numbers[0]
-# max_num = numbers[0]
-# for num in numbers:
-#         total += num
-#         if num > max_num:
-#             max_num = num
-#
-#         if num < min_num:
-#            min_num = num
-# print(numbers)
-# print(total)
-# print(min_num)
-# print(max_num)
-# name = input("Enter your name: ")
-# age = int(input("Enter your age: "))
-# city = input("Enter your city: ")
-# user = {
-#     "name": name,
-#     "age": age,
-#     "city": city
-# }
-# for key, value in user.items():
-#     print(key, value)
-
-# number = float(input("Enter a number: "))
-# res = number.is_integer()
-# x = bin(int(number))
-# a = hex(int(number))
-# b = oct(int(number))
-# rounded = round(number, 2)
-#
-# print("Is integer: ", res)
-# print("Binary: ", x)
-# print("Hex: ", a)
-# print("Oct: ", b)
-# print("Rounded: ", rounded)
-# print("Ratio: ", number.as_integer_ratio())
-
-# numbers = list(map(int, input("Enter a numbers: ").split(",")))
-# max_number = numbers[0]
-# max_index = 0
-# for i in range(len(numbers)):
-#     if numbers[i] > max_number:
-#         max_number = numbers[i]
-#         max_index = i
-# print(max_number)
-# print(max_index)
-
-# number = int(input("Enter number: "))
-#
-# original = number
-#
-# # 1. Збираємо всі парні цифри у перевернутому порядку
-# even_digits = 0
-#
-# temp = number
-# while temp > 0:
-#     temp, digit = divmod(temp, 10)
-#
-#     if digit % 2 == 0:
-#         even_digits = even_digits * 10 + digit
-#
-# # 2. Збираємо нове число, замінюючи парні цифри
-# new_number_reversed = 0
-#
-# temp = original
-# while temp > 0:
-#     temp, digit = divmod(temp, 10)
-#
-#     if digit % 2 == 0:
-#         new_digit = even_digits % 10
-#         even_digits //= 10
-#     else:
-#         new_digit = digit
-#
-#     new_number_reversed = new_number_reversed * 10 + new_digit
-#
-# # 3. Перевертаємо назад, бо збирали справа наліво
-# result = 0
-#
-# while new_number_reversed > 0:
-#     new_number_reversed, digit = divmod(new_number_reversed, 10)
-#     result = result * 10 + digit
-# print(result)
-
-# num = [1, 2, 3, 4, 5, 6]
-# num = [1, 2, 3, 4, 5]
-# num = [1]
-# num = []
-# y = (len(num) + 1) // 2
-# print([num[:y]] + [num[y:]])
-#
-# text = input("Enter a text:")
-# retr = text[::-1]
-# num = []
-# for i in retr:
-#     if i not in num:
-#         num.append(i)
-# print("".join(num))
-
-# lst_1 = [10, 1, 10]
-# max_value = max(lst_1)
-# new_lst = []
-# for num in lst_1:
-#     if num < max_value:
-#         new_lst.append(num)
-#
-# if new_lst:
-#     max_value_two = max(new_lst)
-#     print(max_value_two)
-# else:
-#     print("Do not have second max")
-
-# text = input("Enter your text: ").lower()
-# vowels = "aeiou"
-# count = 0
-# for letter in text:
-#     if letter in vowels:
-#         count +=1
-# print(count)
-
-# i = 1
-# while i <= 5:
-#     j = 5
-#     for j in range(i):
-#         print(i, end='')
-#         j -= 1
-#     print()
-#     i += 1
-#
-#
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-# new_lst = []
-# for num in numbers:
-#     if num % 2 == 0:
-#         new_lst.append(num * 2)
-#     else:
-#         new_lst.append(num * 3)
-# print(new_lst)
-#
-# new_lst = [num * 2 if num % 2 == 0 else num * 3 for num in numbers]
-# print(new_lst)
-
-# numbers = [2, 4, 6, 8, 10, 12, 14, 16]
-# new_lst = []
-#
-# for i, num in enumerate(numbers[2:7], start=2)
-#     if i % 2 == 0:
-#         new_lst.append(num * 2)
-#     else:
-#         new_lst.append(num * 3)
-#
-# print(new_lst)
-
 import random
-# number_comp = random.randint(1,10)
-# while True:
-#     number_people = int(input("enter the number:"))
-#     if number_people > number_comp:
-#         print("Too high")
-#     elif number_people < number_comp:
-#         print("Too low")
-#     else:
-#         print("You win")
-#         break
-
-# player_wins = 0
-# computer_wins = 0
-# while True:
-#     user = input("Enter rock, paper or scissors: ")
-#     computer = random.choice(["rock", "paper", "scissors"])
-#     if user == "exit":
-#         print("Exiting")
-#         break
-#     if user not in ("rock", "paper", "scissors"):
-#         print("Invalid choice")
-#         continue
-#     print(f"Computer chose: {computer}")
-#     if user == computer:
-#         print("Draw")
-#     elif ((user == "rock" and computer == "scissors") or
-#           (user == "paper" and computer == "rock") or
-#           (user == "scissors" and computer == "paper")):
-#         player_wins += 1
-#         print("Player wins!")
-#     else:
-#         computer_wins += 1
-#         print("Computer wins!")
-# print(f"Player wins: {player_wins}")
-# print(f"Computer wins: {computer_wins}")
-
-# balance = 1000
-# while True:
-#     print( """1. Check balance
-# 2. Deposit money
-# 3. Withdraw money
-# 4. Exit""".strip())
-#     choice = int(input("Enter your choice: "))
-#     if choice == 1:
-#         print(f"Your balance is: {balance}")
-#     elif choice == 2:
-#         money = int(input("Enter amount: "))
-#         if money <= 0:
-#             print("Invalid amount")
-#             continue
-#         balance += money
-#         print(f"Your balance is: {balance}")
-#     elif choice == 3:
-#         money = int(input("Enter amount: "))
-#         if money <= 0:
-#             print("Invalid amount")
-#             continue
-#         if money <= balance:
-#             balance -= money
-#             print(f"your balance is: {balance}")
-#         else:
-#             print("Not enough money")
-#     elif choice == 4:
-#         print("Goodbye!")
-#         break
-#     else:
-#         print("Invalid choice")
-# import string
-# tasks = ["Buy milk", "Learn Python", "Go gym"]
-# while True:
-#     print("""1. Show tasks
-# 2. Add task
-# 3. Delete task
-# 4. Exit""".strip())
-#     choice = input("Enter your choice: ").strip()
-#     if not choice:
-#         print("Invalid choice")
-#         continue
-#     if not choice.isdigit():
-#         print("Choice must be digits")
-#         continue
-#     choice = int(choice)
-#     if choice == 1:
-#         if not tasks:
-#             print("No tasks")
-#         else:
-#             for i, enter in enumerate(tasks, start=1):
-#                 print(f"{i}. {enter}")
-#     elif choice == 2:
-#         enter = input("Enter task: ").strip()
-#         tasks.append(enter)
-#         print("Task added")
-#     elif choice == 3:
-#         if not tasks:
-#             print("No tasks to delete")
-#             continue
-#         for i, enter in enumerate(tasks, start=1):
-#             print(f"{i}. {enter}")
-#         number = input("Enter task number: ").strip()
-#         if not number or not number.isdigit():
-#             print("Invalid number")
-#             continue
-#         number = int(number)
-#         if number < 1 or number > len(tasks):
-#             print("Invalid number")
-#             continue
-#         delete_task = tasks.pop(number - 1)
-#         print(f"Deleted task: {delete_task}")
-#
-#     elif choice == 4:
-#         print("Goodbye!")
-#         break
-#     else:
-#         print("Invalid choice")
-# import string
-# text = input("Enter a string: ").title()
-# result = "#"
-# for i in text:
-#     if i not in string.punctuation and i != " ":
-#         result += i
-# print(result)
-
+player_hp = 100
+monster_hp = 100
+while player_hp > 0 and monster_hp > 0:
+    print("""1. Attack
+2. Heal
+3. Run""")
+    choice = input("Enter a choice: ").lower()
+    if choice == "attack":
+        attack = random.randint(10, 25)
+        critical_hit = random.randint(1, 10)
+        if critical_hit == 5:
+            attack *= 2
+            print("CRITICAL HIT!")
+        print("You hit monster for", attack, "damage")
+        monster_hp -= attack
+        print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+        if monster_hp <= 0:
+            print("Monster defeated!")
+            break
+        elif monster_hp < 30:
+            attack = random.randint(10, 30)
+        else:
+            attack = random.randint(5, 20)
+        print("Monster hits you for", attack, "damage")
+        player_hp -= attack
+        if player_hp <= 0:
+            print("Game over")
+            print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+            break
+        print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+    elif choice == "heal":
+        heal = random.randint(15, 30)
+        old_hp = player_hp
+        player_hp += heal
+        if player_hp > 100:
+            player_hp = 100
+        real_hp = player_hp - old_hp
+        print(f"You healed for", real_hp, "HP")
+        attack = random.randint(5, 20)
+        print("Monster hits you for", attack, "damage")
+        player_hp -= attack
+        if player_hp <= 0:
+            print("Game over")
+            print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+            break
+        print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+    elif choice == "run":
+        print("You ran away!")
+        print(f"Player HP:", player_hp, "Monster HP:", monster_hp)
+        break
+    else:
+        print("Invalid choice")
