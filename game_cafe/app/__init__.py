59608470d62145a648a6ecd7ff2ea1aeeb84064c
Katrin <katabrovkova952@gmail.com>
2026-06-15 15:35:49 +0400

    update file

---
+++ game_cafe/app/__init__.py
@@ -0,0 +1,9 @@
+from .errors import (
+    NotVaccinatedError,
+    VaccineError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+
+from .cafe import Cafe
+from .main import go_to_cafe
---
+++ game_cafe/app/cafe.py
@@ -0,0 +1,27 @@
+from game_cafe.app import (
+    NotVaccinatedError,
+    VaccineError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+
+import datetime
+
+
+class Cafe:
+    def __init__(self, name):
+        self.name = name
+
+    def visit_cafe(self, visitor: dict):
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("Visitor is not vaccinated")
+
+        vaccine = visitor["vaccine"]
+
+        if vaccine["expiration_date"] < datetime.date.today():
+                raise OutdatedVaccineError("Vaccine is outdated")
+
+        if not visitor.get("wearing_a_mask"):
+            raise NotWearingMaskError("Visitor is not wearing a mask")
+
+        return f'Welcome to {self.name}'
